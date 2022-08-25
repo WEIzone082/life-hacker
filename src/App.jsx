bcea6e392827685bc97ebbb2e97b2ca7c6f33a58
Jeffrey He <jeffrey@digisalad.cool>
2022-08-25 12:10:53 +0800

    home page init

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import Index from "./pages/Index"
+
+import "./assets/index.sass"
+
+// here I used router v6 ,no switch ,only Routes
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+    return (
+        <div className="App">
+            <Routes>
+                <Route path="/" element={<Index></Index>}></Route>
+            </Routes>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/pages/Index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Index = ()=>{
+    return(
+        <div className="page page-index">
+            <h1>home</h1>
+        </div>
+    )
+}
+
+export default Index
